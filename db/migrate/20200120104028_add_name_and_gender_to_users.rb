cd088c23291c1b8d7053d6b9bb71abdd9d0d29dd
shin usuda <sasasa.24.blue@gmail.com>
2020-01-20 20:54:57 +0900

    migrate database

---
+++ db/migrate/20200120104028_add_name_and_gender_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameAndGenderToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :name, :string, null: false
+    add_column :users, :gender, :string, null: false
+  end
+end
