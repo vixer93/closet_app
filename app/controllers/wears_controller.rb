7342bd36decb5ffdd8acda88b30b2e1f270fdd31
shin usuda <sasasa.24.blue@gmail.com>
2020-01-21 12:03:02 +0900

    add create action in wears controller

--- app/controllers/wears_controller.rb
+++ app/controllers/wears_controller.rb
@@ -1,4 +1,14 @@
 class WearsController < ApplicationController
   def index
   end
+
+  def create
+    @wear = Wear.new(wear_params)
+    @wear.save ? (render :json) : (render :index)
+  end
+
+  private
+  def wear_params
+    params.require(:wear).permit(:image, :color, :type, :brand)
+  end
 end
