b47ae93f3c46c4c2b4c2b630476019d12c5dd84f
shin usuda <sasasa.24.blue@gmail.com>
2020-02-10 22:43:08 +0900

    add column wear brightness and chroma

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_064659) do
+ActiveRecord::Schema.define(version: 2020_02_10_110909) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_02_09_064659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.float "brightness"
+    t.float "chroma"
     t.index ["user_id"], name: "index_wears_on_user_id"
   end
 
