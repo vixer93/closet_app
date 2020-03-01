313f564724302289790ba833988112f09ffd00d8
shin usuda <sasasa.24.blue@gmail.com>
2020-03-01 22:20:55 +0900

    create user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :wears
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+    validates :gender
+    validates :email, uniqueness: {case_sensitive: false},
+                      format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :password
+  end
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+ factory :user do
+   email                 {"kkk@gmail.com"}
+   password              {"00000000"}
+   password_confirmation {"00000000"}
+   name                  {"taro"}
+   gender                {"male"}
+ end
+end
