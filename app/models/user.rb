90aa882484b21c1f7d9fa72ac8bd83d6ef66ee3c
shin usuda <sasasa.24.blue@gmail.com>
2020-03-03 09:33:20 +0900

    fix user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :name
-    validates :gender
-    validates :email, uniqueness: {case_sensitive: false},
-                      format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
-    validates :password
-  end
+  validates :email, uniqueness: {case_sensitive: false},
+                    format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  validates :name, :gender, presence: true
 end
